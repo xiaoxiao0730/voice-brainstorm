d3ea64d12cff60a01597f0553b83a54ca3495c3c
Melody <xiaoxiao_chen@brown.edu>
2026-07-02 15:52:31 +0800

    new changes

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  ssr: {
+    noExternal: true,
+  },
   plugins: [
     tanstackStart({
       server: { entry: "server" },
